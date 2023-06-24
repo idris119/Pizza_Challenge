081ffb72d66ed6ad689d7af9d07d5fd594da8191
Joseph Mburu <josephmacharia575@gmail.com>
2023-06-24 19:41:57 +0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+peppinos = Restaurant.create(name: 'peppinos ', address: 'westlands, Nairobi')
+javahouse = Restaurant.create(name: 'ParkInn', address: 'Parklands, UpperHill')
+
+cheese_pizza = Pizza.create(name: 'Cheese', ingredients: 'Dough, Tomato Sauce, Cheese')
+chicken_pizza = Pizza.create(name: 'chicken', ingredients: 'Dough, Tomato Sauce, Cheese, chicken, Pepperoni')
+
+RestaurantPizza.create(restaurant: peppinos, pizza: cheese_pizza, price: 10)
+RestaurantPizza.create(restaurant: javahouse, pizza: chicken_pizza, price: 12)
