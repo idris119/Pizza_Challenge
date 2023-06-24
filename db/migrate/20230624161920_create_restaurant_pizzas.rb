ef75addb538e372d823af602e25c81fdf2b90aee
Joseph Mburu <josephmacharia575@gmail.com>
2023-06-24 19:30:35 +0300

    modified models

--- db/migrate/20230624161920_create_restaurant_pizzas.rb
+++ db/migrate/20230624161920_create_restaurant_pizzas.rb
@@ -3,8 +3,8 @@ class CreateRestaurantPizzas < ActiveRecord::Migration[6.1]
     create_table :restaurant_pizzas do |t|
       t.references :restaurant, null: false, foreign_key: true
       t.references :pizza, null: false, foreign_key: true
-      t.decimal3 :price
-      t.decimal3 :price
+      #t.decimal:price
+      t.decimal:price
 
       t.timestamps
     end
